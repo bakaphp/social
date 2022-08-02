b47991eb23de8beaf0a59dd5e3410fad7484a0c0
Manuel Pellerano <pelleranomanuel@gmail.com>
2022-08-02 09:59:23 -0400

    add relationship to model UserMessagesActivities

--- src/Models/UserMessagesActivities.php
+++ src/Models/UserMessagesActivities.php
@@ -7,14 +7,33 @@ namespace Kanvas\Social\Models;
 class UserMessagesActivities extends BaseModel
 {
     public int $user_messages_id;
+    public string $entity_namespace;
     public int $from_entity_id;
     public string $type;
     public ?string $username = null;
     public string $text;
     
     /**
+     * Initialize relationship after fetch
+     * since we need entity_namespace info.
+     *
+     * @return void
+     */
+    public function afterFetch()
+    {
+        $this->hasOne(
+            'from_entity_id',
+            $this->entity_namespace,
+            'id',
+            [
+                'reusable' => true,
+                'alias' => 'entityData'
+            ]
+        );
+    }
 
-    * Initialize method for model.
+    /**
+     * Initialize method for model.
      */
     public function initialize()
     {
