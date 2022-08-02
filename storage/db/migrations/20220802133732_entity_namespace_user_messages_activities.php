cf7a276b551ab0da97f80d3fa52bc2c0693b1a2c
Manuel Pellerano <pelleranomanuel@gmail.com>
2022-08-02 09:44:48 -0400

    add entity_namespace to userMessagesActivity table

---
+++ storage/db/migrations/20220802133732_entity_namespace_user_messages_activities.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class EntityNamespaceUserMessagesActivities extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        $this->table('user_messages_activities')
+            ->addColumn('entity_namespace', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+                'collation' => 'latin1_swedish_ci',
+                'encoding' => 'latin1',
+                'after' => 'from_entity_id',
+            ])
+            ->save();
+    }
+}
