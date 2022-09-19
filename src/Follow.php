79ff7823469ecd31a24583789012a3f513236557
FredPeal <frederickpeal@gmail.com>
2022-09-19 14:00:48 -0400

    fix: from entity id

--- src/Follow.php
+++ src/Follow.php
@@ -242,7 +242,7 @@ class Follow
             ]
         ]);
         if ($lastActivity->entity_namespace && class_exists($lastActivity->entity_namespace)) {
-            $entityData = $lastActivity->entity_namespace::findFirst($this->from_entity_id);
+            $entityData = $lastActivity->entity_namespace::findFirst($lastActivity->from_entity_id);
             $username = $entityData->displayname ?? $entityData->name;
         }
         $username = $username ?? '';
